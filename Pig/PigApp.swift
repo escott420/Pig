70a9708315a675dd75a03c99a725f74749602379
Ethan <ethan.scott2@icloud.com>
2023-10-25 09:42:33 -0500

    Initial Commit

---
+++ Pig/PigApp.swift
@@ -0,0 +1,17 @@
+//
+//  PigApp.swift
+//  Pig
+//
+//  Created by Ethan Scott on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct PigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
