2ea18a002b91ee6d52c453bf157d557cd19a7ee4
Ethan <ethan.scott2@icloud.com>
2023-11-01 10:56:26 -0500

    Added winner notification

--- Pig/ContentView.swift
+++ Pig/ContentView.swift
@@ -13,36 +13,60 @@ struct ContentView: View {
     @State private var gameScore = 0
     @State private var randomValue = 0
     @State private var rotation = 0.0
+    @State private var gameOver = false
     var body: some View {
-        ZStack{
-            Color.gray.opacity(0.7).ignoresSafeArea()
-            VStack {
-                Image("Pig").resizable().frame(width: 150, height: 150)
-                CustomText(text:"Pig")
-                Image("pips \(randomValue)")
-                    .resizable()
-                    .frame(width: 150, height: 150)
-                    .rotation3DEffect(.degrees(rotation), axis: (x: 1, y: 1, z: 0))
-                    .padding(50)
-                
-                CustomText(text:"Turn Score: \(turnScore)" )
-                HStack {
-                    Button("Roll") {
-                chooseRandom(times: 3)
-                        withAnimation(.interpolatingSpring(stiffness: 10, damping: 2)) {
-                            rotation += 360
+        NavigationView {
+            ZStack{
+                Color.gray.opacity(0.7).ignoresSafeArea()
+                VStack {
+                    Image("Pig").resizable().frame(width: 150, height: 150)
+                    CustomText(text:"Pig")
+                    Image("pips \(randomValue)")
+                        .resizable()
+                        .frame(width: 150, height: 150)
+                        .rotation3DEffect(.degrees(rotation), axis: (x: 1, y: 1, z: 0))
+                        .padding(50)
+                    
+                    CustomText(text:"Turn Score: \(turnScore)" )
+                    HStack {
+                        Button("Roll") {
+                            chooseRandom(times: 3)
+                            withAnimation(.interpolatingSpring(stiffness: 10, damping: 2)) {
+                                rotation += 360
+                            }
                         }
-                    }
-                    .buttonStyle(CustomButtonStyle())
-                    Button("Hold") {
+                        .buttonStyle(CustomButtonStyle())
+                        Button("Hold") {
+                            gameScore += turnScore
+                            endTurn()
+                            withAnimation(.easeInOut(duration: 1)){
+                                rotation += 360
+                            }
+                            if gameScore >= 100 {
+                                gameOver = true
+                            }
+                        }
                         
+                        .buttonStyle(CustomButtonStyle())
                     }
-                    .buttonStyle(CustomButtonStyle())
+                    CustomText(text:"Game Score: \(gameScore)" )
+                    NavigationLink("How to play", destination: InstructionsView())
+                        .font(Font.custom("Marker Felt", size: 24))
+                        .padding()
+                    Spacer()
                 }
-                CustomText(text:"Game Score: \(gameScore)" )
-                Spacer()
+                .padding()
+                
             }
-            .padding()
+            .alert(isPresented: $gameOver, content: {
+                Alert(title: Text("You won the game!"), dismissButton:
+                        .destructive(Text("Play again"), action: {
+                            withAnimation(Animation.default) {
+                                gameScore = 0
+                                gameOver = false
+                            }
+                        }))
+            })
         }
     }
     func endTurn() {
